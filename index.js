1a08ed8b1988ddd9224b63a9bd347a76b237838e
Dan DeFelippi <dan@driverdan.com>
2013-12-22 16:15:29 -0600

    Proper use of socket.io client, pin socket version, add send summary timer

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 require('coffee-script');
 
 var CGMinerClient = require('cgminer');
+var io = require('socket.io-client');
 
-var HOST = '10.0.0.7';
-var PORT = 4028;
+// host path
+var HOST = 'http://10.0.0.4:8080';
+var NAME = 'miner1';
 
-var client = new CGMinerClient(HOST, PORT);
+// connection to remote host
+var socket = io.connect(HOST);
 
-client.summary().then(function(results) {
-  console.log('Result', results);
-}, function(err) {
-  console.log('Error', err);
-});
+console.log('Connected to %s', HOST);
+
+// cgminer client
+var client = new CGMinerClient();
+
+// Send summary data to the host
+var sendSummary = function () {
+  client.summary().then(function(data) {
+    if (data && data.SUMMARY && data.SUMMARY.length) {
+      socket.emit('miner:summary', {id: NAME, summary: data.SUMMARY[0]});
+    } else {
+      console.log('error with summary data');
+    }
+  }, function(err) {
+    console.log('Error', err);
+  });
+};
+
+setInterval(sendSummary, 5000);
+
+console.log('sendSummary started');
