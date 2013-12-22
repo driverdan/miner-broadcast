58df066c58c1caa9f04be9a9d37c4d44f8c2a490
Dan DeFelippi <dan@driverdan.com>
2013-12-22 15:28:56 -0600

    Add coffeescript for cgminer, call command correctly

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Coffeescript required for cgminer lib
+require('coffee-script');
+
 var CGMinerClient = require('cgminer');
 
 var HOST = '10.0.0.7';
@@ -5,8 +8,8 @@ var PORT = 4028;
 
 var client = new CGMinerClient(HOST, PORT);
 
-client.COMMAND('summary').then(function(results) {
-  console.log(results);
+client.summary().then(function(results) {
+  console.log('Result', results);
 }, function(err) {
-  console.log(err);
+  console.log('Error', err);
 });
