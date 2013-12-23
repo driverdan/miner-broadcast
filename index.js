1409c82542b1fe89f9564ff7aefa5c7241c81818
Dan DeFelippi <dan@driverdan.com>
2013-12-22 18:14:09 -0600

    Start interval after connecting, clear on disconnect

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ var io = require('socket.io-client');
 var HOST = 'http://10.0.0.4:8080';
 var NAME = 'miner1';
 
-// connection to remote host
-var socket = io.connect(HOST, {
-  // Increase delay and reconnect attempts
-  // We want to keep trying to reconnect for as long as is reasonable
-  'reconnection delay': 1000,
-  'max reconnection attempts': 1000
-});
-
-socket.on('connect', function () {
-  console.log('connected to %s', HOST);
-});
-socket.on('disconnect', function () {
-  console.log('disconnected');
-});
-socket.on('reconnecting', function (delay, attempts) {
-  console.log('Attempt %d to reconnect', attempts);
-});
-
 // cgminer client
 var client = new CGMinerClient();
 
@@ -33,7 +15,7 @@ var client = new CGMinerClient();
 var sendSummary = function () {
   client.summary().then(function(data) {
     if (data && data.SUMMARY && data.SUMMARY.length) {
-      socket.emit('miner:summary', {id: NAME, summary: data.SUMMARY[0]});
+      socket.broadcast.emit('miner:summary', {id: NAME, summary: data.SUMMARY[0]});
     } else {
       console.log('error with summary data');
     }
@@ -42,6 +24,29 @@ var sendSummary = function () {
   });
 };
 
-setInterval(sendSummary, 5000);
+// connection to remote host
+var socket = io.connect(HOST, {
+  // Increase delay and reconnect attempts
+  // We want to keep trying to reconnect for as long as is reasonable
+  'reconnection delay': 1000,
+  'max reconnection attempts': 1000
+});
 
-console.log('sendSummary started');
+var sendSummaryInterval;
+
+socket.on('connect', function () {
+  console.log('connected to %s', HOST);
+
+  sendSummaryInterval = setInterval(sendSummary, 5000);
+  console.log('sendSummary started');
+});
+socket.on('disconnect', function () {
+  console.log('disconnected');
+
+  if (sendSummaryInterval === 0 || sendSummaryInterval) {
+    clearInterval(sendSummaryInterval);
+  }
+});
+socket.on('reconnecting', function (delay, attempts) {
+  console.log('Attempt %d to reconnect', attempts);
+});
